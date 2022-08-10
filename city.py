0a4cba21f9c0bdaddd155270ed464fb5b053581a
Asada19 <livencor@gmail.com>
2022-08-10 11:43:35 +0600

    Some updates with functions

--- city.py
+++ city.py
@@ -1,70 +1,95 @@
 import csv
-from os import link
 import requests
 from bs4 import BeautifulSoup
 
-
-url = 'https://ru.wikipedia.org/wiki/Города_Киргизии#Бывшие_города'
-response = requests.get(url)
-soup = BeautifulSoup(response.text, 'lxml')
-table = soup.find('div', class_='mw-parser-output').find('table') 
-
-
-tags = table.findAll('img').find('src')
-print(tags)
+def get_url():
+    url = 'https://ru.wikipedia.org/wiki/Города_Киргизии#Бывшие_города'
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, 'lxml')
+    table = soup.find('div', class_='mw-parser-output').find('table') 
+    return table
 
 
 """
 создание колонок 
 """
-head = []
-for th in table.find('tr'):
-    head.append(' '.join(th.text.split()))
+def create_head(table):
+    head = []
+    for th in table.find('tr'):
+        head.append(' '.join(th.text.split()))
 
-head = head[1:-1:2]
-head.append('Ссылка')
+    head = head[1:-1:2]
+    head.append('Ссылка')
+    return head
 
 
 """
 Сбор ссылок на города
 """
 links = []
-for i in table.find_all('tr'):
-    for j in i.find_all('a')[0:1]:
-        links.append('https://ru.wikipedia.org/' + j.get('href'))
-links = links[1:]
+
+def get_links(links):
+    for i in table.find_all('tr'):
+        for j in i.find_all('a')[0:1]:
+            links.append('https://ru.wikipedia.org/' + j.get('href'))
+
+    links = links[1:]
+    return links
 
 
 """
 Города и информация о них
 """
 column = []
-for i in table.find_all('tr')[1:]:
-    dt = []
-    for j in i:
-        dt.append(j.text.replace('\n', ''))
-    dt = dt[1:-1:2]
-    column.append(dt)
+
+def city_data(column: list):
+    for i in table.find_all('tr')[1:]:
+        dt = []
+        for j in i:
+            dt.append(j.text.replace('\n', ''))
+        dt = dt[1:-1:2]
+        column.append(dt)
 
 
 """ 
 Добавление ссылки к информации о городе
 """
-for index, col in enumerate(column):
-    col.append(links[index])
+def add_links(links, column):
+    for index, col in enumerate(column):
+        col.append(links[index])
 
 
-""" 
+"""     
 Добавление Заголовок от колонки
 """
-column.insert(0, head)
+def form_data(header, column):
+    header.remove('Флаг, герб')
+    for i in column:
+        for x in i:
+            if x == ' ':             
+                i.remove(x)
+            elif x == '':
+                i.remove(x)
+
+    column.insert(0, header)
+    return column
 
 
 """ 
 Сохронение данных в файл CSV
 """
-with open('city.csv', 'w') as file:
-    writer = csv.writer(file)
-    for row in column:
-        writer.writerow(row)
+def write_to_csv(data):
+    with open('city.csv', 'w') as file:
+        writer = csv.writer(file)
+        for row in data:
+            writer.writerow(row)
 
+
+
+table = get_url()
+header = create_head(table)
+get_links(links)
+city_data(column)
+add_links(links, column)
+data = form_data(header, column)
+write_to_csv(data)
